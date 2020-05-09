31e600f1fffd2d32f5e03086f20ab5c99cbd7d02
long nguyễn ngọc thanh <longnnt99@gmail.com>
2020-05-09 17:53:46 +0700

    demo http api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // const urlPizza = "http://ec2-13-229-51-153.ap-southeast-1.compute.amazonaws.com:8080/product/all"
-const urlPizza = "http://ec2-54-254-198-35.ap-southeast-1.compute.amazonaws.com:8080/product/4"
+const urlPizza = "https://ec2-54-254-198-35.ap-southeast-1.compute.amazonaws.com:8080/product/4"
 
 const urlDrinks = "http://ec2-54-254-198-35.ap-southeast-1.compute.amazonaws.com:8080/product/2"
 
